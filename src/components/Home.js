e4cdcb69f48d03651d27f4e139077b786166a98a
Hong Minh <minh.nguyen3@be.com.vn>
2021-08-08 23:31:13 +0700

    new transaction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,57 @@
 import { useState } from 'react';
 import {useCookies} from 'react-cookie';
+import TransactionList from './transaction/TransactionList';
 function Home(){
     const [cookies, setCookie, removeCookie] = useCookies(['token']);
-    // if (cookies.token !== ""){
-    //     setLoginSuccess(true);
-    // }
+    const [logs, setLogs] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
+    if(!isLoaded){
+        fetch('http://localhost:8080/api/v1/log?begin=2019-01-01&end=2022-01-01', {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + cookies.token,
+            },
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            setIsLoaded(true);
+            if(data && data.meta && data.meta.code === 200){
+                setLogs(data.data);
+            }
+        });
+    }
+    const DUMMY_DATA = [
+        {
+            'id': 1,
+            'money': 100,
+            'detail': 'this is detail',
+            'tag': 'ahihihi',
+                  
+        },
+        {
+            'id': 2,
+            'money': 150,
+            'detail': 'this is detail 2',
+            'tag': 'lol',
+                  
+        },
+        {
+            'id': 3,
+            'money': 300,
+            'detail': 'this is detail 3',
+            'tag': 'HM',
+                  
+        },
+    ]
     if(cookies.username && cookies.username !== "") {
         return (
-            <h1> Hello {cookies.username}</h1>
+            <div>
+                <h1> Hello {cookies.username}</h1>
+                <TransactionList data={logs}/>
+            </div>
         )
     }
 
--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -1,6 +1,62 @@
+import { useState } from 'react';
+import {FloatingLabel, Form, Button} from 'react-bootstrap';
+import {useCookies} from 'react-cookie';
+
 function NewTracsaction(){
+    
+    const [money, setMoney] = useState(0);
+    const [tag, setTag] = useState("");
+    const [desc, setDesc] = useState("");
+    const [cookies] = useCookies([]);
+    async function addTransactionHandler(event){
+        event.preventDefault();
+        let body = {
+            'money': parseInt(money),
+            'tag': tag,
+            'detail': desc,
+        }
+        await fetch('http://localhost:8080/api/v1/log', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + cookies.token,
+            },
+            body: JSON.stringify(body),
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            if(data && data.meta && data.meta.code === 200){
+                window.location.href = "/";
+            }
+        })
+    }
+
     return(
-        <h1> this is new transaction page </h1>
+        <div>
+            <Form>
+                <FloatingLabel
+                    controlId="floatingMoney"
+                    label="Money"
+                    className="mb-3"
+                >
+                    <Form.Control type="number" placeholder="1000" onChange={(e) => setMoney(e.target.value)} />
+                </FloatingLabel>
+                <FloatingLabel 
+                    controlId="floatingTag" 
+                    label="Tag">
+                    <Form.Control type="text" placeholder="Eat" onChange={(e) => setTag(e.target.value)}/>
+                </FloatingLabel>
+                <FloatingLabel 
+                    controlId="floatingDesc" 
+                    label="Description">
+                    <Form.Control type="text" placeholder="Eat luch" onChange={(e) => setDesc(e.target.value)}/>
+                </FloatingLabel>
+                <Button variant="primary" type="submit" onClick={addTransactionHandler}>
+                    Add
+                </Button>
+            </Form>
+        </div>
     )
 }
 
